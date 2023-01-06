b1f8bced8f627084c9243f67660d4e6b30b75556
Daniel Winzen <daniel@danwin1210.de>
2023-01-06 20:41:03 +0100

    Update translation files

--- update-translation.sh
+++ update-translation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xgettext -o locale/main-website.pot `find . -iname '*.php'`
+xgettext --from-code UTF-8 -o locale/main-website.pot `find . -iname '*.php'`
 for translation in `find locale -iname '*.po'`; do msgmerge -U "$translation" locale/main-website.pot; msgfmt -o ${translation:0:-2}mo "$translation"; done
